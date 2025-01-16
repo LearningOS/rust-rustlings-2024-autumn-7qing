6a2aee3cf92c7a73d27afee8967356c14da7b296
Yyx-laptop <3454523412@qq.com>
2025-01-17 00:41:17 +0800

    1

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,18 +16,38 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+pub trait Grade {
+    fn format_grade(&self) -> String;
+}
 
-pub struct ReportCard {
-    pub grade: f32,
+// 为 f32 实现 Grade 特征
+impl Grade for f32 {
+    fn format_grade(&self) -> String {
+        self.to_string()
+    }
+}
+
+// 为 String 实现 Grade 特征
+impl Grade for String {
+    fn format_grade(&self) -> String {
+        self.clone()
+    }
+}
+
+pub struct ReportCard<T: Grade> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: Grade> ReportCard<T> {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name,
+            &self.student_age,
+            self.grade.format_grade()
+        )
     }
 }
 
@@ -52,7 +72,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
